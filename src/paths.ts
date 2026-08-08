0f412b0a09e4f4f6511de709700fe755777a404e
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2026-08-08 17:01:48 +0200

    Next path fix

--- src/paths.ts
+++ src/paths.ts
@@ -7,16 +7,16 @@ export function getPermissionGlobalDir(home = homedir()): string {
 	return join(home, ".pi", "agent", PER_DIR);
 }
 
-export function getPiLabGlobalTmpDir(name?: string, home = homedir()): string {
+export function getPermissionsGlobalTmpDir(name?: string, home = homedir()): string {
 	const tmpDir = join(getPermissionGlobalDir(home), "tmp");
 	return name ? join(tmpDir, name) : tmpDir;
 }
 
-export function getPiLabLocalDir(cwd = process.cwd()): string {
+export function getPermissionsLocalDir(cwd = process.cwd()): string {
 	return join(cwd, ".pi", PER_DIR);
 }
 
-export function getPiLabLocalTmpDir(name?: string, cwd = process.cwd()): string {
-	const tmpDir = join(getPiLabLocalDir(cwd), "tmp");
+export function getPermissionsLocalTmpDir(name?: string, cwd = process.cwd()): string {
+	const tmpDir = join(getPermissionsLocalDir(cwd), "tmp");
 	return name ? join(tmpDir, name) : tmpDir;
 }
